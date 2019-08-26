ec90eda9eedfc378ef275bfdbb9923a955295ff3
kourouya <1376483126@qq.com>
2019-08-26 19:48:17 +0800

    .

--- src/com/kourou/collection/Iterator/dEMO2.java
+++ src/com/kourou/collection/Iterator/dEMO2.java
@@ -1,9 +1,20 @@
 package com.kourou.collection.Iterator;
 
+import java.util.List;
+import java.util.Vector;
+
 /**
  * @Author： kourou
  * @Data： 2019/8/24 22:04
  * @Description：
  */
 public class dEMO2 {
+    public static void main(String[] args) {
+        List<String> list = new Vector<>();
+        list.add("hello");
+        list.add("hello");
+        list.add("bit");
+        System.out.println(list);
+    }
+
 }
