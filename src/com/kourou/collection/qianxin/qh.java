f0b09a317a8aa5e43a5f4303cf1b0fd3aa4101a4
kourouya <1376483126@qq.com>
2019-09-13 23:12:23 +0800

    .

--- src/com/kourou/collection/qianxin/qh.java
+++ src/com/kourou/collection/qianxin/qh.java
@@ -1,9 +1,16 @@
 package com.kourou.collection.qianxin;
 
+import java.util.Scanner;
+
 /**
  * @Author： kourou
  * @Data： 2019/9/11 23:35
  * @Description：
  */
 public class qh {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
 }
+
