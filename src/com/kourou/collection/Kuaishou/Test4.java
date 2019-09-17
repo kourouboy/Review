705a18dfc4da48a686e7f8bc0d8db3bce0e1a958
kourouya <1376483126@qq.com>
2019-09-17 23:24:55 +0800

    .

---
+++ src/com/kourou/collection/Kuaishou/Test4.java
@@ -0,0 +1,12 @@
+package com.kourou.collection.Kuaishou;
+
+/**
+ * @Author： kourou
+ * @Data： 2019/9/17 23:22
+ * @Description：N字
+ */
+public class Test4 {
+    public static void main(String[] args) {
+
+    }
+}
