5aba3fcc3ec19dffabdb61cf2d08a1516e87c0d3
kourouya <1376483126@qq.com>
2019-09-21 23:56:15 +0800

    .

--- src/com/kourou/collection/Kuaishou/Test4.java
+++ src/com/kourou/collection/Kuaishou/Test4.java
@@ -1,5 +1,7 @@
 package com.kourou.collection.Kuaishou;
 
+import java.util.Scanner;
+
 /**
  * @Author： kourou
  * @Data： 2019/9/17 23:22
@@ -7,6 +9,11 @@ package com.kourou.collection.Kuaishou;
  */
 public class Test4 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 1;i < 100; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
